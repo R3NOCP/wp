9d6a914512ee375c7c3236ac368b6ce25f181011
Reno Postema <postemareno@gmail.com>
2019-05-20 14:43:41 +0200

    Add header

--- wp-content/themes/custom/header.php
+++ wp-content/themes/custom/header.php
@@ -1,1 +1,14 @@
-<h1>header</h1>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php bloginfo( 'name' ); ?></title>
+</head>
+
+<body <?php body_class(); ?>>
+
+<header class="site-header">
+    <h1><?php bloginfo( 'name' ); ?></h1>
+    <h4><?php bloginfo( 'description' ); ?></h4>
+</header>
