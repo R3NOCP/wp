2cc21929df74656af46856bcd810c0df4bea79a8
Reno Postema <postemareno@gmail.com>
2019-05-24 09:40:58 +0200

    Register sidebar widget

--- wp-content/themes/custom/functions.php
+++ wp-content/themes/custom/functions.php
@@ -7,3 +7,18 @@ function custom_theme_assets() {
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
 
 register_nav_menus( [ 'primary' => __( 'Primary Menu' ) ] );
+
+add_action( 'widgets_init', 'sidebar_default' );
+function sidebar_default() {
+    register_sidebar(
+        array(
+            'id'            => 'primary',
+            'name'          => __( 'Page sidebar' ),
+            'description'   => __( 'Page sidebar' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        )
+    );
+}
