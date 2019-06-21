3de5660ad2e0f61dec8c4165beead5f3b30adfac
Reno Postema <postemareno@gmail.com>
2019-06-21 12:24:48 +0200

    Refresh City Data - Request Liveweer

--- wp-content/themes/custom/js/dropdown.js
+++ wp-content/themes/custom/js/dropdown.js
@@ -1,4 +1,5 @@
 function myFunction() {
   var x = document.getElementById("citySelect").value;
   document.getElementById("city").innerHTML = "You selected: " + x;
+  refreshWeatherData(x);
 }
--- wp-content/themes/custom/js/liveweer.js
+++ wp-content/themes/custom/js/liveweer.js
@@ -1,36 +1,41 @@
-const app = document.getElementById('liveweer')
+const app = document.getElementById('liveweer');
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
-app.appendChild(container)
+app.appendChild(container);
 
-var xhr = new XMLHttpRequest()
-xhr.open('GET', 'https://weerlive.nl/api/json-data-10min.php?key=f97777689c&locatie=groningen', true)
-xhr.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(xhr.responseText)
-  if (xhr.status >= 200 && xhr.status < 400) {
-    data.liveweer.forEach(liveweer => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
+refreshWeatherData('groningen');
 
-      const h2 = document.createElement('h2')
-      h2.textContent = liveweer.plaats
+function refreshWeatherData(cityName){
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', 'https://weerlive.nl/api/json-data-10min.php?key=f97777689c&locatie=' + cityName, true)
+    xhr.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(xhr.responseText)
+        if (xhr.status >= 200 && xhr.status < 400) {
+            data.liveweer.forEach(liveweer => {
+                const card = document.getElementById("weather-info");
+                card.innerHTML = '';
+                card.setAttribute('class', 'card');
 
-      const h1 = document.createElement('h1')
-      h1.textContent = liveweer.temp
+                const h2 = document.createElement('h2');
+                h2.textContent = liveweer.plaats;
 
-      container.appendChild(card)
-      card.appendChild(h2)
-      card.appendChild(h1)
+                const h1 = document.createElement('h1');
+                h1.textContent = liveweer.temp;
 
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Oops!`
-    app.appendChild(errorMessage)
-  }
+                container.appendChild(card);
+                card.appendChild(h2);
+                card.appendChild(h1);
+
+            })
+        } else {
+            const errorMessage = document.createElement('marquee');
+            errorMessage.textContent = `Oops!`;
+            app.appendChild(errorMessage);
+        }
+    }
+
+    xhr.send();
 }
-
-xhr.send()
