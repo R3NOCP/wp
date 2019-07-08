b4cdcce6c21b2383dcc305bb63d2c35d76b719f5
Reno Postema <postemareno@gmail.com>
2019-07-08 15:08:15 +0200

    Add footer widgets

--- wp-content/themes/custom/footer.php
+++ wp-content/themes/custom/footer.php
@@ -1,8 +1,31 @@
 <footer class="site-footer">
     <p><?php bloginfo( 'name' ) ?></p>
+    <div id="footer-sidebar" class="secondary">
+<div id="footer-sidebar1">
+<?php
+if(is_active_sidebar('footer-sidebar-1')){
+dynamic_sidebar('footer-sidebar-1');
+}
+?>
+</div>
+<div id="footer-sidebar2">
+<?php
+if(is_active_sidebar('footer-sidebar-2')){
+dynamic_sidebar('footer-sidebar-2');
+}
+?>
+</div>
+<div id="footer-sidebar3">
+<?php
+if(is_active_sidebar('footer-sidebar-3')){
+dynamic_sidebar('footer-sidebar-3');
+}
+?>
+</div>
+</div>
 </footer>
 </div> </div> <!-- closing <div class=container"> -->
-  
+
 <?php wp_footer() ?>
 </body>
 </html>
